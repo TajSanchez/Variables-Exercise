873f7f970f0039a7f2a592ff0948bf8a00f6a0ef
Taj <tajmarie92@gmail.com>
2023-09-28 11:49:44 -0500

    Completed Variable assignment

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Taj";
+            int myAge = 31;
+            char myInitial = 'T';
+            bool isCool = true;
+            double favNumber = 79.5;
+            decimal myAccount = 500.00m;
+
+            Console.WriteLine($"Hello my name is {myName}, I am {myAge} years old. The first letter in my name starts with {myInitial}. My favorite number is {favNumber}, and I have ${myAccount} in my wallet. All that tells me that I'm petrry cool! {isCool}");
+            
         }
     }
 }
